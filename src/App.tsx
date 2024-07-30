595ce62f18230aa8a551793decad657b22c272c9
Kacegz <90635502+Kacegz@users.noreply.github.com>
2024-07-30 11:35:50 +0200

    Add middle section

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,8 @@ import form3 from './assets/form3.png';
 import carousel1 from './assets/carousel1.png';
 import carousel2 from './assets/carousel2.png';
 import carousel3 from './assets/carousel3.png';
+import middle1 from './assets/middle1.png';
+import middle2 from './assets/middle1.png';
 
 function App() {
     const [carousel, setCarousel] = useState([carousel1, carousel2, carousel3]);
@@ -92,6 +94,85 @@ function App() {
                         Fast shipping | Easy returns | Special discount for
                         students
                     </p>
+                </div>
+                <div className={styles.middleContent}>
+                    <img src={middle1} alt="" />
+                    <div>
+                        <h2>
+                            Curious about the <span>StockX</span> Mystery Box?
+                        </h2>
+                        <div>
+                            <p>
+                                Experience the thrill of unboxing cutting-edge
+                                gadgets with Stockx Mystery Boxes. Each box is a
+                                treasure trove of high-quality electronics,
+                                handpicked to elevate your tech game.
+                            </p>
+                            <p>
+                                Expect the unexpected! Our boxes are packed with
+                                the latest gadgets, from smartphones to gaming
+                                consoles, ensuring you get the best tech
+                                surprises every time.
+                            </p>
+                        </div>
+                    </div>
+                    <div>
+                        <h2>Why is everyone buzzing about these boxes?</h2>
+                        <div>
+                            <p>
+                                StockX Mystery Boxes are not just about
+                                products; they’re about the experience. Imagine
+                                the excitement of unveiling top-tier electronics
+                                at a fraction of the cost.
+                            </p>
+                            <p>
+                                Our boxes include a variety of premium tech
+                                items, making it a must-have for every tech
+                                enthusiast. Join the buzz and see why everyone
+                                is raving about the unbeatable value and
+                                surprise factor of Stockx Mystery Boxes!
+                            </p>
+                        </div>
+                    </div>
+                    <img src={middle2} alt="" />
+                </div>
+                <div className={styles.bottomBanner}>
+                    <div>
+                        <h3>How can I win?</h3>
+                        <p>
+                            Thanks to your membership, you also participate in
+                            our contest to win the iPhone15. We sellect a winner
+                            among every 600 participants. The next winner will
+                            be selected on 06.10.2023, and will be notified
+                            directly by e-mail. If the reward is out of stock,
+                            the winner will receive a similar product of equal
+                            or greater value instead.
+                        </p>
+                    </div>
+                    <div>
+                        <h3>How can I win?</h3>
+                        <p>
+                            Thanks to your membership, you also participate in
+                            our contest to win the iPhone15. We sellect a winner
+                            among every 600 participants. The next winner will
+                            be selected on 06.10.2023, and will be notified
+                            directly by e-mail. If the reward is out of stock,
+                            the winner will receive a similar product of equal
+                            or greater value instead.
+                        </p>
+                    </div>
+                    <div>
+                        <h3>How can I win?</h3>
+                        <p>
+                            Thanks to your membership, you also participate in
+                            our contest to win the iPhone15. We sellect a winner
+                            among every 600 participants. The next winner will
+                            be selected on 06.10.2023, and will be notified
+                            directly by e-mail. If the reward is out of stock,
+                            the winner will receive a similar product of equal
+                            or greater value instead.
+                        </p>
+                    </div>
                 </div>
             </div>
         </>
